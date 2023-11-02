f425f0b10839e9c3dc662466ecad933ef7e786b1
Bryant Mateus <bryant.mateus@gmail.com>
2023-11-02 12:46:36 -0500

    install react js and tailwind css and initial root and auth folders

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+
+        {/* private routes (only if signed-in) */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
